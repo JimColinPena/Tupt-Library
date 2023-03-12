380ec70e0a4fee5841da465bae854c7284a4986a
JimColinPena <jimcolin.pena@gmail.com>
2023-03-13 02:39:56 +0800

    03-13 deployment

--- backend/app.js
+++ backend/app.js
@@ -54,5 +54,17 @@ app.use('/api/v1', user);
 app.use('/api/v1', notification);
 app.use('/api/v1', evaluation);
 
+if (process.env.NODE_ENV !== 'PRODUCTION') 
+    require('dotenv').config({ path: 'backend/config/config.env' })
+
+
+    if (process.env.NODE_ENV === 'PRODUCTION') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    })
+}
+
 app.use(errorMiddleware);
 module.exports = app
